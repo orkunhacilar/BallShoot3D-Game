7e6a154f7ae71c25d76a51ae6135d1e332c88fa0
Orkun Hacılar <orkun.hacilar@gmail.com>
2023-09-27 21:15:30 +0300

    94 9

---
+++ BallShoot3D/Assets/AnaMenuManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AnaMenuManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (PlayerPrefs.HasKey("Level"))
+            SceneManager.LoadScene(PlayerPrefs.GetInt("Level"));
+        else
+            PlayerPrefs.SetInt("Level", 1);
+            SceneManager.LoadScene(1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
